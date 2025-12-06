987f3ab5f503052131785362227449048adbc1a5
Furkan Albayrak <furkanalb42@gmail.com>
2025-12-06 20:24:08 +0300

    store.ts dosyasi zustand yapisi kuruldu

---
+++ lib/store.ts
@@ -0,0 +1,67 @@
+import { Product } from "@/types";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface CartItem extends Product {
+    quantity: number;
+}
+
+interface CartState {
+    items: CartItem[];
+    addItem: (product: Product, quantity: number) => void;
+    removeItem: (productId: string) => void;
+    updateQuantity: (productId: string, quantity: number) => void;
+    clearCart: () => void;
+    getCartTotal: () => number;
+}
+
+export const useCartStore = create<CartState>()(
+    persist(
+        (set, get) => ({
+            items: [],
+            addItem: (product, quantity) => set((state) => {
+                const existingItem = state.items.find((item) => item.id === product.id);
+
+                if (existingItem) {
+                    return {
+                        items: state.items.map((item) =>
+                            item.id === product.id
+                                ? { ...item, quantity: item.quantity + quantity }
+                                : item
+                        )
+                    };
+                }
+
+                return { items: [...state.items, { ...product, quantity }] };
+            }),
+
+            removeItem: (productId) => set((state) => ({
+                items: state.items.filter((item) => item.id !== productId)
+            })),
+
+            updateQuantity: (productId, quantity) => set((state) => ({
+                items: state.items.map((item) =>
+                    item.id === productId
+                        ? { ...item, quantity }
+                        : item
+                )
+            })),
+
+            clearCart: () => set({ items: [] }),
+
+            getCartTotal: ()=> {
+                const items = get().items;
+                return items.reduce((total, item) => {
+                    if(item.unit === 'kg'){
+                        return total + (item.price * (item.quantity / 1000));
+                    }
+                    return total + (item.price * item.quantity);
+                }, 0);
+            }
+        }),
+        
+        {
+            name: 'sua-tarim-cart',
+        }
+    )
+);
