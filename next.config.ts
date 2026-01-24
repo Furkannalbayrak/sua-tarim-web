fedae77a8ecef7b92d94ac402432112990ed6579
Furkan Albayrak <furkanalb42@gmail.com>
2026-01-24 19:08:39 +0300

    next.config.ts Image icindeki resimler icin izinler verildi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io', // Eğer gelecekte CMS kullanırsan diye
+      },
+      // {
+      //   protocol: 'https',
+      //   hostname: '**', // Geçici olarak tüm kaynaklara izin verelim (geliştirme aşaması için kolaylık)
+      // },
+      {
+        protocol: 'https',
+        hostname: 'gtfewjaifroyoprplblp.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
